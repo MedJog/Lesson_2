6e34f9e40dc66b5d6dbf7b28f333584fbd0cb70e
Svetlana <medjog@gmail.com>
2023-03-28 17:37:47 +0400

    Решение задачи

--- Num03/Program.cs
+++ Num03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 1 || a > 7) 
+{
+    Console.WriteLine("Введите другое число.");
+}
+if (a > 0 && a < 6) 
+{
+    Console.WriteLine("Этот день не является выходным.");
+} 
+
+if ( a == 6 || a == 7) 
+{
+Console.WriteLine("Да, это выходной день!");
+}
