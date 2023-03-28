835b689eaf888848b64f17d2a06a549ef8818829
Svetlana <medjog@gmail.com>
2023-03-28 17:09:58 +0400

    Добавлена папка Num02

---
+++ Num02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 && num > -100) {
+    Console.WriteLine("Третьей цифры нет");
+}
+else {
+    
+}
+
+
+
+// Console.WriteLine("");
